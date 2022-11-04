aa6247f40e455da9e0063d25aae9c5a9e8c8d299
Alfonsodela <89443967+Alfonsodela@users.noreply.github.com>
2022-11-04 14:24:54 +0100

    finished about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,15 @@
 import styled from '@emotion/styled';
-import { AboutContainer, AboutInner, AboutLayout, Competencies } from '../styles/layouts/AboutLayout';
+import {
+	AboutContainer,
+	AboutInner,
+	AboutLayout,
+	Competencies,
+	ImgProfile,
+	Paragraph,
+	Profile
+} from '../styles/layouts/AboutLayout';
 import { HomeTitle } from '../styles/layouts/AboutLayout';
-import foto from '../assets/Foto.jpg'
+import foto from '../assets/Foto.jpg';
 
 const About = () => {
 	return (
@@ -9,13 +17,27 @@ const About = () => {
 			<AboutContainer>
 				<HomeTitle>About Me</HomeTitle>
 				<AboutInner>
-					<div className='profile'>
-            <img src={foto}/>
-						<p>Foto</p>
-					</div>
-					<div className='description'>
-						<p>Descripción</p>
-					</div>
+					<Profile>
+						<ImgProfile >
+							<img src={foto}/>
+						</ImgProfile>
+						<p>UBICACION: Sevilla, España</p>
+					</Profile>
+					<Paragraph>
+						Hola, mi nombre es Will y actualmente soy un estudiante de Hamilton
+						College que busca obtener una licenciatura. en Ciencias de la
+						Computación. En los últimos 10 años, la codificación se ha
+						convertido en una de mis pasiones, específicamente en el ámbito del
+						desarrollo web. He dedicado mi tiempo a adquirir competencia en
+						todas las complejidades del desarrollo web para mejorar como
+						desarrollador y solucionador de problemas. Soy capaz de crear sitios
+						web limpios y funcionales, así como marcas profesionales y diseño
+						gráfico. Soy competente en mis habilidades de frontend, backend y
+						diseño hasta el punto de poder entregar productos con confianza a
+						mis clientes sabiendo que son de alta calidad. Disfruto ayudar a las
+						marcas a establecer su presencia digital y desarrollar experiencias
+						de usuario agradables.
+					</Paragraph>
 				</AboutInner>
 			</AboutContainer>
 			<Competencies>
--- src/styles/layouts/AboutLayout.jsx
+++ src/styles/layouts/AboutLayout.jsx
@@ -14,11 +14,13 @@ export const AboutLayout = styled.div`
 	background-color: var(--color-black);
 `;
 
-export const HomeTitle = styled.span`
+export const HomeTitle = styled.h1`
 	color: var(--color-white);
 	background-color: var(--color-black);
 	white-space: nowrap;
 	font-family: Gilroy-Extrabold;
+	margin-top: 50px;
+	margin-bottom: 20px;
 
 	${mobileUp} {
 		font-size: 28px;
@@ -42,10 +44,45 @@ export const AboutInner = styled.div`
 
 	margin-top: 50px;
 	text-align: left;
+	color: var(--color-white);
+	font-family: 'Poppins', sans-serif;
+	font-size: 1.25rem;
 `;
 
 export const Profile = styled.div`
-	background-color: rgba(0, 0, 0, 0.15);
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	padding: 40px;
+	margin-right: 50px;
+	border-radius: 6px;
+	background-color: #1f1f1f;
+`;
+
+export const ImgProfile = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 225px;
+	height: 225px;
+	white-space: nowrap;
+	background-color: transparent;
+	border-radius: 50%;
+	border: 4px solid #e91e63;
+	box-shadow: 0 0 0 6px rgb(233 30 99 / 25%);
+	margin-bottom: 18px;
+`;
+
+export const Paragraph = styled.p`
+	width: 60%;
+	margin: auto auto auto 25px;
+	font-family: 'Poppins', sans-serif;
+	font-weight: 100;
+	font-size: 1.25rem;
+	line-height: 1.6;
+	word-spacing: 5px;
+	color: var(--color-form);
 `;
 
 export const Competencies = styled.div`
@@ -55,6 +92,7 @@ export const Competencies = styled.div`
 	align-items: center;
 
 	width: 100%;
+	margin-top: 125px;
 
 	background-color: var(--color-pink);
 `;
