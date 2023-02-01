7020aa7621c406737c15a9f6f368a38ced7f903b
Alfonsodela <89443967+Alfonsodela@users.noreply.github.com>
2023-02-01 12:59:36 +0100

    Change reactDom in main

--- public/src/main.jsx
+++ public/src/main.jsx
@@ -1,7 +1,7 @@
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'
 
-const container = document.getElementById('root');
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 createRoot(container).render(<App/>)
